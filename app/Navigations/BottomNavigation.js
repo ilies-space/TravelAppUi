59f85d4f609df0adbda10c54b8e748c992df1ad9
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-29 21:51:20 +0100

    BotomBarIcons

--- app/Navigations/BottomNavigation.js
+++ app/Navigations/BottomNavigation.js
@@ -5,16 +5,65 @@ import Explore from '../Screens/Explore';
 import Travel from '../Screens/Travel';
 import Saved from '../Screens/Saved';
 import Profile from '../Screens/Profile';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function BottomNavigation() {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Explore" component={Explore} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Saved" component={Saved} />
-      <Tab.Screen name="Travel" component={Travel} />
+    <Tab.Navigator tabBarOptions={{showLabel: false}}>
+      <Tab.Screen
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="compass-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Explore"
+        component={Explore}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="bookmark-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Profile"
+        component={Profile}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons name="airplane" color={color} size={size} />
+          ),
+        }}
+        name="Saved"
+        component={Saved}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="bag-personal-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+        name="Travel"
+        component={Travel}
+      />
     </Tab.Navigator>
   );
 }
