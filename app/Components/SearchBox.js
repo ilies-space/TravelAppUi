01015449a7c3214f1cb44b3c6e69b74114248164
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-01-30 17:04:10 +0100

    SearchBox 👌

---
+++ app/Components/SearchBox.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import {TextInput} from 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function SearchBox() {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+      <View
+        style={{
+          // paddingLeft: -30,
+          elevation: 1,
+          position: 'absolute',
+          left: 20,
+        }}>
+        <MaterialCommunityIcons name="map-search" color={'grey'} size={20} />
+      </View>
+      <TextInput
+        style={{
+          height: 50,
+          marginHorizontal: 10,
+          backgroundColor: 'white',
+          borderRadius: 20,
+          padding: 10,
+          paddingLeft: 50,
+          flex: 1,
+        }}
+        placeholder={'Search for destinations'}
+      />
+    </View>
+  );
+}
--- app/Screens/Explore.js
+++ app/Screens/Explore.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import HorizontalList from '../Components/HorizontalList';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import SearchBox from '../Components/SearchBox';
 
 export default function Explore() {
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: '#ecf0f1',
+      }}>
+      <View
+        style={{
+          marginVertical: 10,
+        }}
+      />
+
+      {/* ----------------------------- */}
+      <SearchBox />
+      {/* ----------------------------- */}
+      <View
+        style={{
+          marginVertical: 5,
+        }}
+      />
+      {/* ----------------------------- */}
       <HorizontalList />
     </View>
   );
